da1a81b18cd5dcf7371bf4c66c3779030b9f7157
klinton.daltio@gmail.com <@daltiosk77>
2024-05-13 18:00:47 -0300

    Aulas 51 a 53

---
+++ src/test/java/academy/devdojo/UserServiceApplicationTests.java
@@ -0,0 +1,26 @@
+package academy.devdojo;
+
+import academy.devdojo.commons.UserUtils;
+import academy.devdojo.domain.User;
+import academy.devdojo.repository.UserData;
+import academy.devdojo.repository.UserHardCodedRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void coontextLoads(){
+
+}
+}
